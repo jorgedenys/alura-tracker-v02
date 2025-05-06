a5fc37465f70d701639f9a6df874dcc365accae7
Jorge Denys <jorgedenys@gmail.com>
2025-05-06 10:47:01 -0300

    Commit inicial

---
+++ src/components/TarefasRealizadas.vue
@@ -0,0 +1,27 @@
+<template>
+    <BoxTarefas>
+        <div class="columns">
+            <div class="column is-7">
+                {{ tarefa.descricao || 'Tarefa sem descrição' }}
+            </div>
+            <div class="column">
+                <CronometroTarefas :tempoEmSegundos="tarefa.duracaoEmSegundos" />
+            </div>
+        </div>
+    </BoxTarefas>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import CronometroTarefas from './CronometroTarefas.vue';
+import ITarefa from '@/interfaces/ITarefa';
+import BoxTarefas from './BoxTarefas.vue';
+
+export default defineComponent({
+    name: 'TarefasRealizadas',
+    components: { CronometroTarefas, BoxTarefas },
+    props: {
+        tarefa: { type: Object as PropType<ITarefa>, required: true }
+    }
+})
+</script>
