c1ae7fa4ce687f6da393db0b199fa81c8bfe0bcb
Jorge Denys <jorgedenys@gmail.com>
2025-05-17 14:06:40 -0300

    Inclusão do projeto na tabela do formulário

--- src/components/TarefasRealizadas.vue
+++ src/components/TarefasRealizadas.vue
@@ -1,8 +1,11 @@
 <template>
     <BoxTarefas>
         <div class="columns">
-            <div class="column is-7">
+            <div class="column is-4">
                 {{ tarefa.descricao || 'Tarefa sem descrição' }}
+            </div>
+            <div class="column is-3">
+                {{ tarefa.projeto.nome }}
             </div>
             <div class="column">
                 <CronometroTarefas :tempoEmSegundos="tarefa.duracaoEmSegundos" />
